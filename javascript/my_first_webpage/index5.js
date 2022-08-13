85d59ddcbaca034087435d0a99e1a4f583826969
dk-wei <darken.vi@hotmail.com>
2022-08-13 14:30:42 -0700

    update

--- javascript/my_first_webpage/index5.js
+++
@@ -1,9 +0,0 @@
-const msg = " This is my \n 'first msg     "
-msg = 'haha'
-console.log(msg)
-console.log(msg[2])
-console.log(msg.includes('my'))
-console.log(msg.startsWith('This'))
-console.log(msg.indexOf('my'));
-console.log(msg.split(' '));
-console.log(msg.split(' ')[1]);
